e11c514b35b72a7399648b89133c24d854e95058
noalero <noalero@post.bgu.ac.il>
2022-04-17 19:33:41 +0300

    little check omri

--- kernel/proc.h
+++ kernel/proc.h
@@ -24,6 +24,7 @@ struct cpu {
   struct context context;     // swtch() here to enter scheduler().
   int noff;                   // Depth of push_off() nesting.
   int intena;                 // Were interrupts enabled before push_off()?
+  int rate = 5;
 };
 
 extern struct cpu cpus[NCPU];
@@ -92,6 +93,10 @@ struct proc {
   int killed;                  // If non-zero, have been killed
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
+  int mean_ticks = 0;
+  int last_ticks = 0;
+  int last_runnable_time;
+
 
   // wait_lock must be held when using this:
   struct proc *parent;         // Parent process
